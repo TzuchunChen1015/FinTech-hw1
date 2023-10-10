9e47f08faac185a337ae598c7acd9eecd63ca205
TzuchunChen1015 <tzuchunchen1015@gmail.com>
2023-10-10 14:35:25 +0800

    add KD value calculation

---
+++ src/kd.py
@@ -0,0 +1,32 @@
+def CalculateKD(pastPriceVec, currentPrice, KDalpha):
+  import numpy as np
+
+  dataLen = len(pastPriceVec)
+  if dataLen <= 8:
+    maxPrice = np.max(np.append(pastPriceVec, currentPrice))
+    minPrice = np.min(np.append(pastPriceVec, currentPrice))
+    if maxPrice == minPrice:
+      return 50, 50
+    else:
+      rsv = (currentPrice - minPrice) / (maxPrice - minPrice)
+      return rsv, rsv
+  else:
+    maxPrice = np.max(pastPriceVec[0:9])
+    minPrice = np.min(pastPriceVec[0:9])
+    rsv = (pastPriceVec[8] - minPrice) / (maxPrice - minPrice)
+    kValue = dValue = rsv
+
+    for idx in range(9, dataLen):
+      maxPrice = np.max(pastPriceVec[idx-8:idx+1])
+      minPrice = np.min(pastPriceVec[idx-8:idx+1])
+      rsv = (pastPriceVec[idx] - minPrice) / (maxPrice - minPrice)
+      kValue = KDalpha * kValue + (1 - KDalpha) * rsv
+      dValue = KDalpha * dValue + (1 - KDalpha) * kValue
+
+    maxPrice = np.max(np.append(pastPriceVec[-8:], currentPrice))
+    minPrice = np.min(np.append(pastPriceVec[-8:], currentPrice))
+    rsv = (currentPrice - minPrice) / (maxPrice - minPrice)
+    kValue = KDalpha * kValue + (1 - KDalpha) * rsv
+    dValue = KDalpha * dValue + (1 - KDalpha) * kValue
+
+    return kValue, dValue
